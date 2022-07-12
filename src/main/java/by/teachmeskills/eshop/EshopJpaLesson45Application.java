c65bcd9c4a3209a6f446150c4d236bcc3c6be7e3
tsarikevich <ma.tsarikevich@gmail.com>
2022-07-12 10:17:18 +0300

    Initial commit

---
+++ src/main/java/by/teachmeskills/eshop/EshopJpaLesson45Application.java
@@ -0,0 +1,13 @@
+package by.teachmeskills.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopJpaLesson45Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopJpaLesson45Application.class, args);
+    }
+
+}
